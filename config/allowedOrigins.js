e5be4226b2a9db66e8a5f7b34082c2412061f01e
chihebgit <chiheb.samti@gmail.com>
2024-07-25 20:25:51 +0200

    fix CORS bug

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://127.0.0.1:5500",
   "http://localhost:5173",
-  "https://urban-roots-six.vercel.app/",
+  "https://urban-roots-six.vercel.app",
 ];
 
 module.exports = allowedOrigins;
