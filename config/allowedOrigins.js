1a2b5ac707b1729701134d74d4397ae132a599f5
chihebgit <chiheb.samti@gmail.com>
2024-07-25 20:22:24 +0200

    add CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://127.0.0.1:5500", "http://localhost:5173"];
+const allowedOrigins = [
+  "https://127.0.0.1:5500",
+  "http://localhost:5173",
+  "https://urban-roots-six.vercel.app/",
+];
 
 module.exports = allowedOrigins;
